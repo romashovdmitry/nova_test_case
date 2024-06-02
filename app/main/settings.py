f6b8065e3d394e6719459f15e46f204d60f5b30a
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2024-06-03 00:29:36 +0400

    update env for ci/cd

--- app/main/settings.py
+++ app/main/settings.py
@@ -11,7 +11,11 @@ SECRET_KEY = getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    str(
+        getenv("HOST")
+    )
+]
 
 INSTALLED_APPS = [
     "django.contrib.auth",
